8540c03774c54231e932dd3b2a54b105aaea5122
Muhammad-Owais-Warsi <warsimuhammadowais@gamil.com>
2026-01-24 19:36:41 +0000

    preview and get checkout session

--- src/api/checkout.rs
+++ src/api/checkout.rs
@@ -17,4 +17,32 @@ impl<'client> CheckoutApi<'client> {
             .request(Method::POST, "/checkouts", query_params, body, ext_path)
             .await
     }
+
+    pub async fn preview(
+        &self,
+        query_params: Option<HashMap<&str, &str>>,
+        body: Option<serde_json::Value>,
+        ext_path: Option<&str>,
+    ) -> Result<ResponseData, DodoError> {
+        self.client
+            .request(
+                Method::POST,
+                "/checkouts/preview",
+                query_params,
+                body,
+                ext_path,
+            )
+            .await
+    }
+
+    pub async fn retreive(
+        &self,
+        query_params: Option<HashMap<&str, &str>>,
+        body: Option<serde_json::Value>,
+        ext_path: Option<&str>,
+    ) -> Result<ResponseData, DodoError> {
+        self.client
+            .request(Method::GET, "/checkouts", query_params, body, ext_path)
+            .await
+    }
 }
